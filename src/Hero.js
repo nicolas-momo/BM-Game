e3a8db5b949f172bbb9ae388f668092ce03ffc56
NickBirrer <nicolasruzonbirrer@gmail.com>
2022-10-25 18:45:32 -0300

    Primeiro commit

---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from 'react';
+
+export const Hero = (props) => {
+  const [counter, setCounter] = useState(props.hp);
+    return (
+        <>
+        <button onClick={ () => setCounter((prevCount) => prevCount -5)}>-</button>
+          <h3>Health: {counter},</h3>
+          <h3>Intelligence: {props.int},</h3>
+          <h3>Mana: {props.mp},</h3>
+          <h3>Damage: {props.dmg},</h3>
+        </>
+  );
+}
